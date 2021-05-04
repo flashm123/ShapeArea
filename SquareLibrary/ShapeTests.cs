82841089e3637b2b2d1d17f665fea3fb60c930f7
flashm123 <n_plk7a@mail.ru>
2021-05-04 20:04:34 +0300

    Add tests

---
+++ SquareLibrary/ShapeTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SquareLibrary
+{
+	[TestFixture]
+	public class ShapeTests
+	{
+		[Test]
+		public void CheckAreaOfRectangle()
+		{
+			var a = 5;
+			var b = 7;
+			var expectedArea = 35;
+			
+			var actualArea = new ShapeFactoryBuilder().Sides(a, b).GetArea();
+			
+			Assert.AreEqual(expectedArea, actualArea);
+		}
+		
+		[Test]
+		public void CheckAreaOfCircle()
+		{
+			var radius = 5;
+			var expectedArea = 5 * 5 * Math.PI;
+			
+			var actualArea = new ShapeFactoryBuilder().Radius(radius).GetArea();
+			
+			Assert.AreEqual(expectedArea, actualArea);
+		}
+		
+		[Test]
+		public void CheckAreaOfTriangle()
+		{
+			var a = 5;
+			var b = 7;
+			var c = 9;
+			var expectedArea = 17.41;
+			
+			var actualArea = new ShapeFactoryBuilder().Sides(a, b, c).GetArea();
+			
+			Assert.AreEqual(expectedArea, Math.Round(actualArea, 2));
+		}
+		
+	}
+}
